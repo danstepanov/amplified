a98bb6eae3369a522ee1737c9df89a852178196f
Dan <danhstepanov@gmail.com>
2013-12-07 15:44:23 -0500

    commit

---
+++ response.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+<?
+	
+	php include 'www_misc/contact_res/formmail.php'; 
+	echo '<br /><br /><a href="'.$_SERVER['HTTP_REFERER'].'">Return</a>'; 
+?>
+</body>
+</html>
--- send.php
+++ send.php
@@ -1,19 +1,16 @@
 <?
-
+print_r(POST[]);
+exit
 $name=$_POST['name'];
 $email=$_POST['email'];
-$message=$_POST['message'];
-
-
-    
+$message=$_POST['message']; 
     $body .= "Name: " . $name . "\n"; 
     $body .= "Email: " . $email . "\n"; 
     $body .= "Message: " . $message . "\n"; 
 
+    
     //replace with your email
-    mail("dan@amplified.us","Daniel Stepanov",$body); 
-
-  
+ mail("dan@amplified.us","Daniel Stepanov",$body);   
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -21,5 +18,4 @@ $message=$_POST['message'];
 <head>
 <script>alert("Your message has been sent successfully. We will contact you shortly.");</script>
 <meta HTTP-EQUIV="REFRESH" content="0; url=index.html"> 
-
 </head>
